327b8fdbb6d696727ca2bc744136e478de1ffb9e
kazarixx <tua.ih8@gmail.com>
2021-09-14 12:15:08 +0000

    [middle]やり直した。編集、削除までできている

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   get 'homes/index'
   resources :books
+  delete 'books/:id' => 'bookss#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
